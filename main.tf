3207c0b18565d88bc266d39f690b49b3ceebde41
assareh <assareh@hashicorp.com>
2020-06-03 10:12:58 -0700

    updating tags on benchmark server

--- main.tf
+++ main.tf
@@ -159,7 +159,9 @@ resource aws_instance "hashicat" {
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
   tags = {
-    Name = "assareh-hashicat-instance"
+    Name = "assareh-hashicat-instance",
+    owner = var.owner,
+    ttl = var.ttl
   }
 }
 
