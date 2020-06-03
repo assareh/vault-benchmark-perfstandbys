c410780e83515b83352e67bb4f4942c16cd13c84
assareh <assareh@hashicorp.com>
2020-06-03 15:28:19 -0700

    redoing benchmark sg

--- main.tf
+++ main.tf
@@ -178,32 +178,32 @@ resource "aws_security_group" "benchmark" {
   vpc_id = aws_vpc.benchmarking.id
 }
 
-resource "aws_security_group_rule" "benchmark_ssh" {
-  security_group_id = aws_security_group.benchmark.id
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["76.93.151.110/32"]
-}
+# resource "aws_security_group_rule" "benchmark_ssh" {
+#   security_group_id = aws_security_group.benchmark.id
+#   type              = "ingress"
+#   from_port         = 22
+#   to_port           = 22
+#   protocol          = "tcp"
+#   cidr_blocks       = ["76.93.151.110/32"]
+# }
 
-resource "aws_security_group_rule" "benchmark_egress" {
-  security_group_id = aws_security_group.benchmark.id
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
+# resource "aws_security_group_rule" "benchmark_egress" {
+#   security_group_id = aws_security_group.benchmark.id
+#   type              = "egress"
+#   from_port         = 0
+#   to_port           = 0
+#   protocol          = "-1"
+#   cidr_blocks       = ["0.0.0.0/0"]
+# }
 
-resource "aws_security_group_rule" "benchmark_vault_elb" {
-  security_group_id        = aws_security_group.benchmark.id
-  type                     = "ingress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = "-1"
-  source_security_group_id = aws_security_group.vault_elb.id
-}
+# resource "aws_security_group_rule" "benchmark_vault_elb" {
+#   security_group_id        = aws_security_group.benchmark.id
+#   type                     = "ingress"
+#   from_port                = 0
+#   to_port                  = 0
+#   protocol                 = "-1"
+#   source_security_group_id = aws_security_group.vault_elb.id
+# }
 
 resource aws_instance "benchmark" {
   ami                         = data.aws_ami.ubuntu.id
